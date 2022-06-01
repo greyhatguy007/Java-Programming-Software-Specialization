965103c1e795f7b85e05d5fe641d9397ee5d59a3
Ritvik Prasad <mritvikprasad@gmail.com>
2022-06-01 21:40:57 +0530

    moved files

---
+++ C1-Programming Foundations with JavaScript, HTML and CSS/week3/js/dochange.js
@@ -0,0 +1,11 @@
+function dochange() {
+    alert("CLICKED BUTTON");
+}
+
+function changebg1() {
+    document.body.style.background = "#ccc";
+}
+
+function changebg2() {
+    document.body.style.background = 'rgb(2, 247, 23)';
+}
