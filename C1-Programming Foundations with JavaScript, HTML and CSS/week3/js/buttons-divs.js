9685b6d85685e7c534caa5e2e52cdcbb0867913f
Ritvik Prasad <mritvikprasad@gmail.com>
2022-06-05 12:31:52 +0530

    modified files

--- C1-Programming Foundations with JavaScript, HTML and CSS/week3/js/buttons-divs.js
+++ C1-Programming Foundations with JavaScript, HTML and CSS/week3/js/buttons-divs.js
@@ -1,3 +1,15 @@
 function alertme() {
     alert("THIS IS A USELESS ALERT");
 }
+
+
+
+function changethemelight() {
+    body = document.getElementById("bod");
+    body.className = "lighttheme";
+}
+
+function changethemedark() {
+
+    body.className = "darktheme";
+}
